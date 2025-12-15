abbd27a49baee267adebc16094d161f4a583123a
Odyseo <shandler_s.e.a@hotmail.es>
2025-12-14 23:41:16 -0500

    imported sys

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import word_count, chartacter_count, sort_char
-
+import sys
 def get_book_text (file_path): 
     with open(file_path) as f:
         file_contents = f.read()
@@ -7,16 +7,18 @@ def get_book_text (file_path):
 
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-    chars = chartacter_count(get_book_text)
-
+    book_path = sys.argv[1]
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count(get_book_text)} total words")
     print("--------- Character Count -------")
-
+    chars = chartacter_count(get_book_text)
     for char, count in sort_char(chars):
         if char.isalpha():
             print(f"{char}: {count}")
